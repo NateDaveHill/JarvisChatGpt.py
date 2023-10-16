0fdbc5d4f40a17c42c23e9cbfd15129df062bf6f
NateDaveHill <nathanhill123@gmail.com>
2023-10-15 22:31:58 -0400

    I have a chatbot in the terminal now.. still need to work on speech recognition and text to speech, but there is progress

--- main.py
+++ main.py
@@ -3,44 +3,48 @@ import speech_recognition as sr
 from gtts import gTTS
 from playsound import playsound
 
-
-openai.api_key = "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"
+# Set your OpenAI API key here
 
 recognizer = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print("Say something:")
-    audio = recognizer.listen(source)
+def recognize_speech():
+    with sr.Microphone() as source:
+        print("Say something:")
+        audio = recognizer.listen(source)
+        print("Recognizing...")
 
-try:
-    recognized_text = recognizer.recognize_google(audio)
-    print("You said:", recognized_text)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print(f"Could not request results from Google Speech Recognition; {e}")
+        try:
+            recognized_text = recognizer.recognize_google(audio)
+            return recognized_text
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+            return ""
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition; {e}")
+            return ""
 
 def text_to_speech(text):
     tts = gTTS(text)
     tts.save("response.mp3")
-    playsound.playsound("response.mp3")
+    playsound("response.mp3")
 
-    while True:
-        user_input = input("You: ")
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=f"You said: {user_input} \nAI:",
-            max_tokens=150
-        )
-        ai_response = response.choice[0].text.strip()
-        print(f"AI: {ai_response}")
+while True:
+    user_input = input("You: ")
 
-        if "exit" in user_input.lower():
-            break
-        if "speak" in user_input.lower():
-            speech = recognizer_speech()
-            print(f"You said: {speech}")
+    if "exit" in user_input.lower():
+        break
 
-        text_to_speech(ai_response)
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"You said: {user_input} \nAI:",
+        max_tokens=150
+    )
+    ai_response = response.choices[0].text.strip()
+    print(f"AI: {ai_response}")
 
-    print("Conversation ended.")
+    if "speak" in user_input.lower():
+        speech = recognize_speech()
+        print(f"You said: {speech}")
+        text_to_speech(speech)
+
+print("Conversation ended.")
